04af3f2768c7c229f84640d6c00e602f4e12899f
Manu Masson <manummasson8@gmail.com>
2025-12-18 13:54:18 +1100

    Update VoiceTree to v1.7.8

--- Casks/voicetree.rb
+++ Casks/voicetree.rb
@@ -1,6 +1,6 @@
 cask "voicetree" do
   version "1.7.8"
-  sha256 "d9aeeab4f4d8f5fabf4c99296933e96c65699ed86913a7befc8d0b305780309f"
+  sha256 "4daa2a976d0f89906a6fe045f630743a95a6b69f548225b9e8b9e42559f0e9e8"
 
   url "https://github.com/voicetreelab/voicetree/releases/download/v#{version}/voicetree.dmg"
   name "VoiceTree"
