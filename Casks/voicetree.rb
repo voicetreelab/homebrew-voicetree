f5cc64106c5127f27da1cab4541948cdd867b811
Manu Masson <manummasson8@gmail.com>
2025-12-23 21:52:59 +1100

    Update VoiceTree to v1.9.2 (multi-arch)

--- Casks/voicetree.rb
+++ Casks/voicetree.rb
@@ -1,8 +1,16 @@
 cask "voicetree" do
-  version "1.8.5"
-  sha256 "0e890c0ae01b659c239221e8c134335d31c100e7d11b4b9dc49c0ed657ff3333"
+  version "1.9.2"
 
-  url "https://github.com/voicetreelab/voicetree/releases/download/v#{version}/voicetree.dmg"
+  on_arm do
+    sha256 "6849897e4f86227f1113c7427e8089e5450d3f7bde326e27da06e789eb7827d5"
+    url "https://github.com/voicetreelab/voicetree/releases/download/v#{version}/voicetree-arm64.dmg"
+  end
+
+  on_intel do
+    sha256 "c25e0e4b437924b4c074b89100261f1151fc17acb4b08d32ba03704a4d49692f"
+    url "https://github.com/voicetreelab/voicetree/releases/download/v#{version}/voicetree-x64.dmg"
+  end
+
   name "VoiceTree"
   desc "Transform voice into navigable concept graphs"
   homepage "https://github.com/voicetreelab/voicetree"
