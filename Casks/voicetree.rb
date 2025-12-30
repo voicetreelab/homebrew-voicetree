6bf70c08b6b401caf589e9a75b5a1d1252c99f7e
Manu Masson <manummasson8@gmail.com>
2025-12-30 21:45:11 +1100

    Update VoiceTree to v1.9.2 (multi-arch)

--- Casks/voicetree.rb
+++ Casks/voicetree.rb
@@ -2,12 +2,12 @@ cask "voicetree" do
   version "1.9.2"
 
   on_arm do
-    sha256 "6849897e4f86227f1113c7427e8089e5450d3f7bde326e27da06e789eb7827d5"
+    sha256 "7cbe7b40f2b1fc30961f259ec6bd52700e10ffc5f63aa7883ea234be5647b7d0"
     url "https://github.com/voicetreelab/voicetree/releases/download/v#{version}/voicetree-arm64.dmg"
   end
 
   on_intel do
-    sha256 "c25e0e4b437924b4c074b89100261f1151fc17acb4b08d32ba03704a4d49692f"
+    sha256 "949b573a6265cd8083f7f0720244ded7401dc7a587cab23b82bc714ddff0c727"
     url "https://github.com/voicetreelab/voicetree/releases/download/v#{version}/voicetree-x64.dmg"
   end
 
